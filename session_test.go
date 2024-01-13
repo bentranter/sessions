6820bafc8213d044ff693bdaf7d2f7ea3603b537
Ben Tranter <ben@bentranter.io>
2024-01-13 17:04:25 -0500

    docs: add example

--- session_test.go
+++ session_test.go
@@ -1,11 +1,78 @@
 package sessions
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 )
+
+func ExampleSession() {
+	// Create a new session manager.
+	session := New(GenerateRandomKey(32), Options{
+		// Set a custom session name (default is "_session").
+		Name: "_example_session_name",
+	})
+
+	// Get and set session data.
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			name := session.Get(r, "name")
+			fmt.Fprintf(w, "Your name is: %s\n", name)
+			return
+		}
+		session.Set(w, r, "name", "Ben")
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	})
+
+	// List and display all session data as JSON.
+	http.HandleFunc("/list", func(w http.ResponseWriter, r *http.Request) {
+		values := session.List(r)
+
+		data, err := json.MarshalIndent(values, "", "  ")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(data)
+	})
+
+	// Delete a flash message.
+	http.HandleFunc("/delete", func(w http.ResponseWriter, r *http.Request) {
+		session.Delete(w, r, "name")
+		http.Redirect(w, r, "/", http.StatusFound)
+	})
+
+	// List and set flash messages.
+	http.HandleFunc("/flash", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			flashes := session.Flashes(w, r)
+
+			data, err := json.MarshalIndent(flashes, "", "  ")
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.Write(data)
+			return
+		}
+
+		if r.URL.Query().Has("flash") {
+			session.Flash(w, r, "notice", "This is a flash message")
+		}
+		http.Redirect(w, r, "/flash", http.StatusSeeOther)
+	})
+
+	// Clear all session data.
+	http.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+		session.Reset(w, r)
+		http.Redirect(w, r, "/", http.StatusFound)
+	})
+}
 
 func TestSessionGetNonNil(t *testing.T) {
 	t.Parallel()
